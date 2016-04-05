b98dfe650315764fddb3d57639267bded39776f2
Leo <leo@users.noreply.github.com>
2016-04-05 18:09:54 +0200

    Don't toggle menu if animating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
   $('#controls .toggle').click(function (event) {
-    $('html').toggleClass('no-overflow')
-    $('#menu').fadeToggle(250).css('display','flex')
-
     event.preventDefault()
+
+    if ($('#menu').is(':animated')) {
+      return
+    }
+
+    $('html').toggleClass('no-overflow')
+    $('#menu').fadeToggle(200).css('display','flex')
   })
 })
