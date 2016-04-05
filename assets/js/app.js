e6454f5c879aed0e4b6e707fdd8295579f43b39d
Leo <leo@users.noreply.github.com>
2016-04-05 17:13:43 +0200

    Finalize menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+  $('#controls .toggle').click(function (event) {
+    $('html').toggleClass('no-overflow')
+    $('#menu').fadeToggle(250).css('display','flex')
 
+    event.preventDefault()
+  })
 })
